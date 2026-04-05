71a4ed6fb59ed6a6b7376d900a5bd9e7262a95df
MugiSus <mmgiss256@gmail.com>
2026-04-06 03:26:50 +0900

    refactor: adjust gradient background scale and update homepage layout and project links

--- src/components/GradientBackground/fragment.frag
+++ src/components/GradientBackground/fragment.frag
@@ -77,12 +77,12 @@ float snoise(vec3 v) {
 }
 
 void main(void) {
-  vec2 pos = (gl_FragCoord.xy + vec2(uTime * 10.0, uTime * -10.0 + uScroll / -4800.0)) / 1200.0;
+  vec2 pos = (gl_FragCoord.xy + vec2(uTime * 10.0, uTime * -10.0 + uScroll / -4800.0)) / 1600.0;
 
   float val = snoise(vec3(pos.x, pos.y, uTime / 100.0 + uScroll / 4800.0));
   float vnorm = (val + 1.0) / 2.0;
 
-  float a = clamp(floor(vnorm * 12.0) / 12.0, 0.0, 1.0) - mod(vnorm, 1.0 / 12.0) * 0.1;
+  float a = clamp(floor(vnorm * 12.0) / 12.0, 0.0, 1.0) - mod(vnorm, 1.0 / 12.0) * 0.2;
   vec3 color = mix(uColor2, uColor1, a);
 
   gl_FragColor = vec4(color, 1.0);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,23 @@
 export default function Home() {
   return (
     <main>
-      <div class="text-4xl flex fixed right-16 top-12 text-emerald-950 flex-col gap-0">
+      <div class="text-4xl flex fixed left-16 top-12 text-emerald-950 flex-col gap-0">
         <p>MUGISUS</p>
         <p>MINATO Masayuki</p>
         <p>湊 真之</p>
       </div>
 
-      <div class="fixed left-16 bottom-16 flex gap-12 flex-col">
-        <ul class="text-3xl text-emerald-950 flex flex-col gap-2">
+      <div class="fixed text-3xl text-emerald-950 left-16 bottom-16 flex gap-12 flex-col">
+        <ul class="flex flex-col gap-2">
           <li>
-            <a href="https://sundial.mugisus.me" class="underline underline-offset-4 decoration-2">Sundial</a>
+            <a href="https://tmacywg.vercel.app" class="underline underline-offset-4 decoration-2">themostaccurateclockyouwouldget</a>
           </li>
           <li>
-            <a href="https://signsystemme.vercel.app" class="underline underline-offset-4 decoration-2">SSM</a>
+            <a href="https://signsystemme.vercel.app" class="underline underline-offset-4 decoration-2">s.s.m.</a>
           </li>
         </ul>
-        <ul class="text-3xl text-emerald-950 flex flex-col gap-2">
+        <a href="https://mugisus.me" class="underline underline-offset-4 decoration-2">WORKS</a>
+        <ul class="flex flex-col gap-2">
           <li>
             <a href="https://github.com/mugisus" class="underline underline-offset-4 decoration-2">GitHub: mugisus</a>
           </li>
