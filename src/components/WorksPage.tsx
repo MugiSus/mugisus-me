4b581fa77f81b864bb462863c38c3d72c1122a50
MugiSus <mmgiss256@gmail.com>
2026-04-07 03:26:57 +0900

    refactor: adjust spacing and layout in WorksPage component

--- src/components/WorksPage.tsx
+++ src/components/WorksPage.tsx
@@ -6,21 +6,15 @@ export default function WorksPage() {
   return (
     <main>
       <ul class='flex min-h-screen flex-col gap-2 p-12 text-3xl'>
-        <li class='sticky top-12 mb-12'>
-          <A href='/' class='flex items-center gap-1'>
+        <li class='sticky top-12 mb-6'>
+          <A href='/' class='flex items-center gap-2'>
             <ArrowLeftIcon stroke-width={1.5} size={32} />
             Back to Home
           </A>
         </li>
-        <li class='mt-auto flex items-center gap-1'>
-          <a href='https://tmacyes.vercel.app'>
-            themostaccurateclockyouveeverseen
-          </a>
-          <ArrowUpRightIcon stroke-width={1.5} size={32} />
-        </li>
         <For each={Array(40).fill(null)}>
           {() => (
-            <li class='flex items-center gap-1'>
+            <li class='ml-10 flex items-center gap-1'>
               <a href='https://signsystemme.vercel.app'>s.s.m.</a>
               <ArrowUpRightIcon stroke-width={1.5} size={32} />
             </li>
