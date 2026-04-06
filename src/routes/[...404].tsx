696a6ba686464120b3255f069f53dfd8a49cc704
MugiSus <mmgiss256@gmail.com>
2026-04-06 22:20:48 +0900

    refactor: update NotFound component styling and enhance Home component with icons for external links

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -3,8 +3,9 @@ import { ArrowLeftIcon } from 'lucide-solid';
 
 export default function NotFound() {
   return (
-    <main class='mx-auto flex h-svh flex-col items-center justify-center gap-4 p-4 text-center'>
-      <h1 class='text-6xl font-thin'>404</h1>
+    <main class='mx-auto flex h-svh items-center justify-center gap-4 p-4 text-center'>
+      <h1 class='text-2xl font-light'>404</h1>
+      <div class='h-6 w-px bg-emerald-950' />
       <A href='/' class='flex items-center gap-1 no-underline!'>
         <ArrowLeftIcon stroke-width={1} size={20} />
         Back to Home
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { A } from '@solidjs/router';
+import { ArrowUpRightIcon } from 'lucide-solid';
 
 export default function Home() {
   return (
@@ -11,24 +12,28 @@ export default function Home() {
 
       <div class='fixed bottom-0 left-0 flex flex-col gap-12 p-12 text-3xl text-emerald-950'>
         <ul class='flex flex-col gap-2'>
-          <li>
+          <li class='flex items-center gap-1'>
             <a href='https://tmacyes.vercel.app'>
               themostaccurateclockyouveeverseen
             </a>
+            <ArrowUpRightIcon stroke-width={1.5} size={32} />
           </li>
-          <li>
+          <li class='flex items-center gap-1'>
             <a href='https://signsystemme.vercel.app'>s.s.m.</a>
+            <ArrowUpRightIcon stroke-width={1.5} size={32} />
           </li>
         </ul>
         <A href='/works' class='w-fit'>
           WORKS
         </A>
         <ul class='flex flex-col gap-2'>
-          <li>
+          <li class='flex items-center gap-1'>
             <a href='https://github.com/mugisus'>GitHub: mugisus</a>
+            <ArrowUpRightIcon stroke-width={1.5} size={32} />
           </li>
-          <li>
+          <li class='flex items-center gap-1'>
             <a href='https://twitter.com/mugisus'>X: mugisus</a>
+            <ArrowUpRightIcon stroke-width={1.5} size={32} />
           </li>
         </ul>
       </div>
